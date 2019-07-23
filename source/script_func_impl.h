cf8eaf304ed651c7ea7f830c3ce38cb0dac70925
René Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2019-07-23 21:21:18 +0200

    Use token paste operator before __VA_ARGS__

--- source/script_func_impl.h
+++ source/script_func_impl.h
@@ -1,5 +1,5 @@
 ﻿
-#define ParamIndexToString(index, ...)				TokenToString(*aParam[(index)], __VA_ARGS__)
+#define ParamIndexToString(index, ...)				TokenToString(*aParam[(index)], ##__VA_ARGS__)
 #define ParamIndexToInt64(index)					TokenToInt64(*aParam[(index)])
 #define ParamIndexToInt(index)						(int)ParamIndexToInt64(index)
 #define ParamIndexToIntPtr(index)					(INT_PTR)ParamIndexToInt64(index)
@@ -37,19 +37,19 @@
 #define ParamIndexToOptionalBOOL(index, def)		ParamIndexToOptionalType(BOOL, index, def)
 #define ParamIndexToOptionalVar(index)				(((index) < aParamCount && aParam[index]->symbol == SYM_VAR) ? aParam[index]->var : NULL)
 
-#define ParamIndexToOptionalStringDef(index, def, ...)	(ParamIndexIsOmitted(index) ? (def) : ParamIndexToString(index, __VA_ARGS__))
+#define ParamIndexToOptionalStringDef(index, def, ...)	(ParamIndexIsOmitted(index) ? (def) : ParamIndexToString(index, ##__VA_ARGS__))
 // The macro below defaults to "", since that is by far the most common default.
 // This allows it to skip the check for SYM_MISSING, which always has marker == _T("").
-#define ParamIndexToOptionalString(index, ...)		(((index) < aParamCount) ? ParamIndexToString(index, __VA_ARGS__) : _T(""))
+#define ParamIndexToOptionalString(index, ...)		(((index) < aParamCount) ? ParamIndexToString(index, ##__VA_ARGS__) : _T(""))
 
 #define ParamIndexToOptionalObject(index)			((index) < aParamCount ? ParamIndexToObject(index) : NULL)
 
 #define _f_param_string(name, index, ...) \
-	TCHAR name##_buf[MAX_NUMBER_SIZE], *name = ParamIndexToString(index, name##_buf, __VA_ARGS__)
+	TCHAR name##_buf[MAX_NUMBER_SIZE], *name = ParamIndexToString(index, name##_buf, ##__VA_ARGS__)
 #define _f_param_string_opt(name, index, ...) \
-	TCHAR name##_buf[MAX_NUMBER_SIZE], *name = ParamIndexToOptionalString(index, name##_buf, __VA_ARGS__)
+	TCHAR name##_buf[MAX_NUMBER_SIZE], *name = ParamIndexToOptionalString(index, name##_buf, ##__VA_ARGS__)
 #define _f_param_string_opt_def(name, index, def, ...) \
-	TCHAR name##_buf[MAX_NUMBER_SIZE], *name = ParamIndexToOptionalStringDef(index, def, name##_buf, __VA_ARGS__)
+	TCHAR name##_buf[MAX_NUMBER_SIZE], *name = ParamIndexToOptionalStringDef(index, def, name##_buf, ##__VA_ARGS__)
 
 #define Throw_if_Param_NaN(ParamIndex) \
 	if (!TokenIsNumeric(*aParam[(ParamIndex)])) \
